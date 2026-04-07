f7de7fc62b55ba9dc6cd13eda6647f1cb87c519f
dev kumar <muskan@example.com>
2026-04-07 11:48:10 +0530

    Fixed Port for Render and update server logic

--- backend/server.js
+++ backend/server.js
@@ -166,7 +166,7 @@ app.post("/scan-timetable", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+app.listen(PORT,'0.0.0.0' () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
